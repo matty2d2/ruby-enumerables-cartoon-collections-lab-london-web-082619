2a86950a8b8151e696012c305e12c7b481447aa6
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-22 11:09:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map {|item| 
+  array.map {|item| item.capitalize
 end
 
 def long_planeteer_calls# code an argument here
